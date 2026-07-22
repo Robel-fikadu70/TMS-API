a3df41c7416d143c991c45d295746114fb3bc837
Ruther.f <robelfikadu70@gmail.com>
2026-07-22 08:06:13 +0300

    add missing namespace on EnrollmentController.

--- TmsApi.Api/Controllers/V2/EnrollmentsController.cs
+++ TmsApi.Api/Controllers/V2/EnrollmentsController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using TmsApi.Application.Enrollments.Commands;
 using TmsApi.Application.Enrollments.Queries;
 
+namespace TmsApi.Api.Controllers.V2;
+
 [ApiController]
 [Route("api/v{version:apiVersion}/enrollments")]
 [ApiVersion("2.0")]
